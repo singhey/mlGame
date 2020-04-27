1237ff45f50e98a0100accb1c08f9d60317f8fdf
Abhishek Singh <singhey2@gmail.com>
2020-04-27 10:45:44 +0530

    Initial Commit

---
+++ precache-manifest.e3bdd5689cfc9eea4dbc67ea8ee1bf34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3ad030da7182bea47d05848636ffff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6341fc85bed02000b996",
+    "url": "/static/js/2.c88c58d2.chunk.js"
+  },
+  {
+    "revision": "71b3ec5f5c6ef88d5410",
+    "url": "/static/js/main.0d49cd34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
